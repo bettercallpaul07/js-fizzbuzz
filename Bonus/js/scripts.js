9c6d58f43a5e76448f7f5ba79ec93143c5444e72
Paolo Rapuano <bettercallpaul07@gmail.com>
2022-12-21 16:35:14 +0100

    Sistemato CSS

--- Bonus/js/scripts.js
+++ Bonus/js/scripts.js
@@ -1,48 +1,30 @@
-/*  for (let i=1; i<=100; i++) {
 
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log("Fizzbuzz", i)
-    }
-
-    else if (i % 3 == 0) {
-    console.log("Fizz", i)}
-
-    else if (i % 5 == 0) {
-    console.log("Buzz", i)
-    }
-
-    else {
-        console.log(i)
-    }
-
-} */
 
 const div = document.querySelector('div.container');
 
 for (let index = 1; index <= 100; index++) {
     
     const newDiv= document.createElement("div");
-    console.log("newDiv", newDiv, typeof newDiv);
     newDiv.innerHTML = `${index}`;
     newDiv.className = `box box-${index}`;
 
     if ((index % 3 == 0) && (index % 5 == 0)) {
         newDiv.innerHTML = `fizzbuzz`;
-        newDiv.className = `fizzbuzz`;
+        newDiv.className = `box fizzbuzz`;
     }
 
     else if (index % 3 == 0) {
         newDiv.innerHTML = `fizz`;
-        newDiv.className = `fizz`;
+        newDiv.className = `box fizz`;
     }
 
     else if (index % 5 == 0) {
         newDiv.innerHTML = `buzz`;
-        newDiv.className = `buzz`;
+        newDiv.className = `box buzz`;
         }
     
     //Con append aggiungo un elemento HTML alla mia DOM
-    //passandogli newDiv (elemento HTML creato con la funzione createElement)
+    //passandogli newDiv (nuovo elemento HTML creato con la funzione createElement)
     div.append(newDiv);
 
 }
